2827c6b026effd8af14fee250fef3796ccff2fbc
agbanusi <53221092+agbanusi@users.noreply.github.com>
2020-06-06 19:33:15 +0100

    Update autofxcm.py

--- autofxcm.py
+++ autofxcm.py
@@ -1,48 +1,59 @@
 import trial1 as tr
 import MetaTrader5 as ft
 import time
+from twilio.rest import Client
+client = Client(process.env.KEY,process.env.SECRET)
 
-def monitor(jb,vf,im,i,hh):
+from_whatsapp='whatsapp:+14155238886'
+#to_whatsapp='whatsapp:+2348073975086'
+
+
+def monitor(jb,vf,im,i,hh,d,to_whatsapp):
   t = 'done'
-  if len(hh) > 0:
+  if d == 1 and len(hh) >0:
     tickett=hh[2]
     tt=ft.positions_get(ticket=tickett)
     trade=tt[0][15]  
-    if trade <=-3 and vf== 0:
+    if trade <=-2.5 and vf== 0:
         ft.Close(im,ticket=tickett)
-        hy=ft.Sell(im,0.06)
+        hy=ft.Sell(im,0.04)
+        client.messages.create(body='Close the Buy loss in '+im+' , I advise you Sell' ,from_=from_whatsapp,to=to_whatsapp)            
         while True:
-            if ft.positions_get(hy[2])[0][15] >= 20:
-                ft.Close(im,hy[2])
+            if (ft.positions_get(hy[2]))[0][15] >= 10:
+                ft.Close(im,ticket=hy[2])
                 break
-            elif ft.positions_get(hy[2])[0][15]<=-5:
-                ft.Close(im,hy[2])
+            elif (ft.positions_get(hy[2]))[0][15]<=-2.5:
+                ft.Close(im,ticket=hy[2])
                 break
         return t
-    elif trade<=-3 and vf== 1:
+            
+    elif trade<=-2.5 and vf== 1:
         ft.Close(im,ticket=tickett)
-        hy=ft.Buy(im,0.06)
+        hy=ft.Buy(im,0.04)
+        client.messages.create(body='Close the Sell loss in '+im+' , I advise you Buy' ,from_=from_whatsapp,to=to_whatsapp)
         while True:
-            if ft.positions_get(hy[2])[0][15] >= 20:
-                ft.Close(im,hy[2])
+            if (ft.positions_get(hy[2]))[0][15] >= 10:
+                ft.Close(im,ticket=hy[2])
                 break
-            elif ft.positions_get(hy[2])[0][15]<=-5:
-                ft.Close(im,hy[2])
+            elif (ft.positions_get(hy[2]))[0][15]<=-2.5:
+                ft.Close(im,ticket=hy[2])
                 break
         return t
     elif trade >= 50 and vf == 0:
         jbb=ft.symbol_info_tick(im)
         time.sleep(2)
         jbg=ft.symbol_info_tick(im)
-        if jbb[2] > (jbg[2]):
-                ft.Close(im,ticket=tickett)
+        if jbb[2] >= (jbg[2]):
+            ft.Close(im,ticket=tickett)
+            client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)
         return t
     elif trade >=50 and vf==1:
         jbb=ft.symbol_info_tick(im)
         time.sleep(2)
         jbg=ft.symbol_info_tick(im)
-        if jbb[1] < (jbg[1]):
-                ft.Close(im,ticket=tickett)
+        if jbb[1] <= (jbg[1]):
+            ft.Close(im,ticket=tickett)
+            client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)            
         return t
     elif 50>trade>=20 and vf==0:
         jbb=ft.symbol_info_tick(im)
@@ -50,6 +61,7 @@ def monitor(jb,vf,im,i,hh):
         jbg=ft.symbol_info_tick(im)
         if jbb[2] > (jbg[2]+0.00005):
             ft.Close(im,ticket=tickett)
+            client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)            
         return t
     elif 50>trade>=20 and vf==1:
         jbb=ft.symbol_info_tick(im)
@@ -57,6 +69,7 @@ def monitor(jb,vf,im,i,hh):
         jbg=ft.symbol_info_tick(im)
         if jbg[1] > (jbb[1]-0.00005):
             ft.Close(im,ticket=tickett)
+            client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)           
         return t
     elif 20>trade>=10 and vf==0:
         jbb=ft.symbol_info_tick(im)
@@ -64,61 +77,74 @@ def monitor(jb,vf,im,i,hh):
         jbg=ft.symbol_info_tick(im)
         if jbb[2] > (jbg[2]+0.000075):
             ft.Close(im,ticket=tickett)
+            client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)            
         return t
     elif 20>trade>=10 and vf==1:
         jbb=ft.symbol_info_tick(im)
         time.sleep(2)
         jbg=ft.symbol_info_tick(im)
         if jbg[1] > (jbb[1]-0.000075):
-            ft.Close(im,ticket=tickett)            
+            ft.Close(im,ticket=tickett)
+            client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)            
         return t
-    elif 10>trade>=2.5 and vf==0:
+    elif 10>trade>=2 and vf==0:
         jbb=ft.symbol_info_tick(im)
         time.sleep(2)
         jbg=ft.symbol_info_tick(im)
         if jbb[2] > (jbg[2]+0.00010):
             ft.Close(im,ticket=tickett)
+            client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)
         return t
-    elif 10>trade>=2.5 and vf==1:
+    elif 10>trade>=2 and vf==1:
         jbb=ft.symbol_info_tick(im)
         time.sleep(2)
         jbg=ft.symbol_info_tick(im)
         if jbg[1] > (jbb[1]-0.00010):
             ft.Close(im,ticket=tickett)
+            client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)
         return t
-    elif trade<=-3:
+    elif trade<=-2.5:
         ft.Close(im,ticket=tickett)
+        client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)
+        return t
     elif trade>=10:
         ft.Close(im,ticket=tickett)
+        client.messages.create(body='Close the profit in '+im ,from_=from_whatsapp,to=to_whatsapp)
+        return t
     else:
         return 'not'
-  elif len(hh)<=0:
+  elif d ==0 and len(hh)<=0:
       print('no trade done at the moment')
       return t
+  else:
+      print('Error occured in getting ticket')
+      return 'not'
       
-def trade(data,interval,data_,im,i,jb,v1,v2,v3,v4,v5,v6,v7):
+def trade(data,interval,data_,im,i,jb,v1,v2,v3,v4,v5,v6,v7,to_whatsapp):
     try:
-        vf= tr.decision(data,interval,data_,v1,v2,v3,v4,v5,v6,v7)
+        bf= tr.decision(data,interval,data_,v1,v2,v3,v4,v5,v6,v7)
         '''level = tr.conf_level(data,interval,data_,v1,v2,v3,v4,v5,v6,v7)[-1]
         if level <0:
             level=-level'''
-        if vf[-1] ==0:
-            hh = ft.Buy(im,0.03)
+        if bf[-1] ==0:
+            hh = ft.Buy(im,0.02)
             print('d-checked')
-            return hh[2],vf[-1],hh
-        elif vf[-1]==1:
-           hh= ft.Sell(im,0.03)
+            client.messages.create(body='Buy '+im ,from_=from_whatsapp,to=to_whatsapp)            
+            return hh[2],bf[-1],hh,1
+        elif bf[-1]==1:
+           hh= ft.Sell(im,0.02)
            print('e-checked')
-           return hh[2],vf[-1],hh
-        elif vf[-1]==2:
-            return 0,vf[-1],[]
+           client.messages.create(body='Sell '+im ,from_=from_whatsapp,to=to_whatsapp)            
+           return hh[2],bf[-1],hh,1
+        elif bf[-1]==2:
+            print('none')
+            return 0,bf[-1],[],0
         else:
              print('Unknown Error Occured!')
-             return 0,vf[-1],[]
+             return 0,bf[-1],[],0
     except KeyError:
          print('no Trade yet')
-         return 0,vf[-1],[]
-            
+         return 0,bf[-1],[],0
             
             
             
